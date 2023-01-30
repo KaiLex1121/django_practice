471756cc7d57278419cb3e7cce64a5af8ebebe82
Lesha Kai <kojjmin@gmail.com>
2023-01-30 14:23:59 +0300

    Added dynamic URL for horoscope app

--- django_projects/my_page/horoscope_pages/urls.py
+++ django_projects/my_page/horoscope_pages/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('leo/', views.show_leo_sign),
-    path('aries/', views.show_aries_sign),
-    path('taurus/', views.show_taurus_sign)
-    ]
+    path('<zodiac_sign>/', views.get_zodiac_sign),
+]
