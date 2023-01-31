efa6dc5612584071c8fec8ab74c0da535668352d
Lesha Kai <kojjmin@gmail.com>
2023-01-31 20:23:56 +0300

    Added redirects to geometry_pages app

--- django_projects/my_page/geometry_pages/views.py
+++ django_projects/my_page/geometry_pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from math import pi
 
 
@@ -9,13 +9,28 @@ def get_rectangle_area(request, width, height):
     return HttpResponse(f"Площадь = {square}")
 
 
+def get_rectangle_area_redirect(request, width, height):
+
+    return HttpResponseRedirect(f'/calculate_geometry/rectangle/{width}/{height}')
+
+
 def get_square_area(request, width):
     square = width * width
 
     return HttpResponse(f'Площадь = {square}')
 
 
+def get_square_area_redirect(request, width):
+
+    return HttpResponseRedirect(f'/calculate_geometry/square/{width}')
+
+
 def get_circle_area(request, radius):
     square = (radius * radius) * pi
 
     return HttpResponse(f'Площадь = {square: .2f}')
+
+
+def get_circle_area_redirect(request, radius):
+
+    return HttpResponseRedirect(f'/calculate_geometry/circle/{radius}')
