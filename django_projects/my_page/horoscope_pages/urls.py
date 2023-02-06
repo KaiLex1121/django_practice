938bb2a45147a169d83b266dbfd4fc14104460a6
Lesha Kai <kojjmin@gmail.com>
2023-02-06 11:23:56 +0300

    Added getting sign by date in horoscope_app

--- django_projects/my_page/horoscope_pages/urls.py
+++ django_projects/my_page/horoscope_pages/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.get_index),
+    path('type/', views.get_type_page),
+    path('type/<element>/', views.get_signs_by_elements, name='signs_by_element'),
+    path('<int:day>/<int:month>/', views.get_sign_by_date),
     path('<int:zodiac_number>/', views.get_zodiac_sign_by_num),
-    path('<str:zodiac_sign>/', views.get_zodiac_sign, name='horoscope_zodiac_sign')
+    path('<str:zodiac_sign>/', views.get_zodiac_sign, name='horoscope_zodiac_sign'),
 ]
--- django_projects/my_page/horoscope_pages/views.py
+++ django_projects/my_page/horoscope_pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-
+from datetime import datetime, date
 
 def get_signs() -> dict:
 
@@ -23,6 +23,96 @@ def get_signs() -> dict:
     return signs
 
 
+def get_signs_elements():
+
+    zodiac_elements = {
+        'fire': ['aries', 'leo', 'sagittarius'],
+        'earth': ['taurus', 'virgo', 'capricorn'],
+        'air': ['gemini', 'libra', 'aquarius'],
+        'water': ['cancer', 'scorpio', 'pisces']
+    }
+
+    return zodiac_elements
+
+
+def get_zodiac_dates():
+
+    zodiac_dates = {
+        1:  {'capricorn': (1, 20),   'aquarius': (21, 31)},
+        2:  {'aquarius': (1, 19),    'pisces': (20, 29)},
+        3:  {'pisces': (1, 20),      'aries': (21, 31)},
+        4:  {'aries': (1, 20),       'taurus': (21, 30)},
+        5:  {'taurus': (1, 21),      'gemini': (22, 31)},
+        6:  {'gemini':  (1, 21),     'cancer': (22, 30)},
+        7:  {'cancer':  (1, 22),     'leo': (23, 31)},
+        8:  {'leo': (1, 21),         'virgo': (22, 31)},
+        9:  {'virgo': (1, 22),       'libra': (23, 30)},
+        10: {'libra': (1, 23),       'scorpio': (24, 31)},
+        11: {'scorpio': (1, 22),     'sagittarius': (23, 30)},
+        12: {'sagittarius': (1, 22), 'capricorn': (23, 31)}
+    }
+
+    return zodiac_dates
+
+
+def get_sign_by_date(request, day, month):
+
+    zodiac_dates = get_zodiac_dates()
+
+    if month in zodiac_dates:
+        signs_in_month = zodiac_dates[month]
+
+        for sign, sign_date in signs_in_month.items():
+
+            if day in range(sign_date[0], sign_date[1]+1):
+
+                redirect_url = reverse('horoscope_zodiac_sign', args=(sign,))
+                break
+
+    return HttpResponseRedirect(redirect_url)
+
+
+def get_type_page(request):
+
+    elements_types = tuple(get_signs_elements())
+
+    elements_with_html_tags = ''
+
+    for element in elements_types:
+
+        redirect_url = reverse('signs_by_element', args=(element,))
+
+        elements_with_html_tags += f'<li> <a href="{redirect_url}"> {element.capitalize()} </a> </li>'
+
+    response = f'<ul> {elements_with_html_tags} </ul>'
+
+    return HttpResponse(response)
+
+
+def get_signs_by_elements(request, element):
+
+    signs_elements = get_signs_elements()
+
+    if element in signs_elements:
+
+        signs_by_element = signs_elements[element]
+
+        elements_with_html_tags = ''
+
+        for sign in signs_by_element:
+
+            redirect_url = reverse('horoscope_zodiac_sign', args=(sign,))
+
+            elements_with_html_tags += f'<li> <a href="{redirect_url}"> {sign.capitalize()} </a> </li>'
+
+        response = f'<ul> {elements_with_html_tags} </ul>'
+
+        return HttpResponse(response)
+
+    else:
+        return HttpResponseNotFound(f'Элемента {element} не существует')
+
+
 def get_index(request):
 
     signs_names = tuple(get_signs())
@@ -31,12 +121,11 @@ def get_index(request):
 
     for sign in signs_names:
         redirect_url = reverse('horoscope_zodiac_sign', args=(sign,))
-        signs_with_html_tags += f'<li> <a href="{redirect_url}"> {sign} </a> </li>'
+        signs_with_html_tags += f'<li> <a href="{redirect_url}"> {sign.title()} </a> </li>'
 
     response = f'<ol> {signs_with_html_tags} </ol>'
 
     return HttpResponse(response)
-
 
 
 def get_zodiac_sign(request, zodiac_sign: str):
